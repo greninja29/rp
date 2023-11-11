4f20e6d3579ccdc0418fae267044502fbffa36ea
Kalyan Reddy <kalyankally29@gmail.com>
2023-11-12 00:36:49 +0530

    updaed simSingle

--- simSingle.py
+++ simSingle.py
@@ -3,7 +3,7 @@ import os
 
 for i in range(80000,500000,5000):
     os.environ['ARG_FROM_PARENT'] = str(i)
-    command_to_run = 'python pacemaker_battery.py'
+    command_to_run = 'python pacemaker_batterySingle.py'
     os.system(command_to_run)
 
 
